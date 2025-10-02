4240c822a9570c58dc8a2ddad50284784b9578f5
Avranil Dhar <179360557+AvranilDhar@users.noreply.github.com>
2025-10-02 13:39:54 +0530

    configure app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,17 @@
 import express from "express";
+import cookieparser from "cookie-parser";
+import { authRouter } from "./routes/auth.route.js";
+const app  = express();
+
+app.use(express.json({
+    limit : "10kb",
+}));
+app.use(express.urlencoded({
+    extended : true,
+    limit : "10kb",
+}));
+app.use(cookieparser());
+
+app.use('/api/auth',authRouter);
+
+export { app };
