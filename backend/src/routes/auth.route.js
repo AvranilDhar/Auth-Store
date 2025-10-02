35a8feb46291a6c9737d844e486cfad7bbc7b379
Avranil Dhar <179360557+AvranilDhar@users.noreply.github.com>
2025-10-03 02:33:26 +0530

    add verify-email route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { signup,login,logout,verifyEmail } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.get('/login',(req,res)=>{
-    res.send('Hello World\n');
-})
+authRouter.post('/signup',signup);
+authRouter.post('/login',login);
+authRouter.post('/logout',logout);
+authRouter.post('/verifyEmail',verifyEmail);
 
 export { authRouter } ;
