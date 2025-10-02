11165117a3d539008ac4c51cefed62865420a405
Avranil Dhar <179360557+AvranilDhar@users.noreply.github.com>
2025-10-02 14:50:43 +0530

    add accesstoken and refreshtoken env variables

--- backend/src/env.js
+++ backend/src/env.js
@@ -4,6 +4,10 @@ const ENV = {
     PORT : process.env.PORT || 5001,
     MONGODB_URI : process.env.MONGODB_URI,
     NODE_ENV : process.env.NODE_ENV,
+    ACCESSTOKEN_SECRET : process.env.ACCESSTOKEN_SECRET,
+    REFRESHTOKEN_SECRET : process.env.REFRESHTOKEN_SECRET,
+    ACCESSTOKEN_EXPIRY : process.env.ACCESSTOKEN_EXPIRY,
+    REFRESHTOKEN_EXPIRY : process.env.REFRESHTOKEN_EXPIRY
 }
 
 export { ENV };
