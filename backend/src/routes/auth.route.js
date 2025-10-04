71d9f199dcd9155df58b967419f326d244a0f964
Avranil Dhar <179360557+AvranilDhar@users.noreply.github.com>
2025-10-04 16:08:06 +0530

    add forgot-password and reset-password route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
-import { signup,login,logout,verifyEmail } from "../controllers/auth.controller.js";
+import { 
+    signup, 
+    login, 
+    logout,
+    verifyEmail, 
+    forgotPassword,
+    resetPassword,
+ } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post('/signup',signup);
 authRouter.post('/login',login);
 authRouter.post('/logout',logout);
-authRouter.post('/verifyEmail',verifyEmail);
+authRouter.post('/verify-email',verifyEmail);
+authRouter.post('/forgot-password',forgotPassword);
+authRouter.post('/reset-password',resetPassword);
+
+
 
 export { authRouter } ;
